65103228fe2a1fc51addbddd36b7cc4e5e83ed7a
pavanthandrangi29 <114342432+pavanthandrangi29@users.noreply.github.com>
2025-08-12 17:48:23 +0530

    Time: 2245 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -0,0 +1,11 @@
+SELECT
+distinct p.product_id,
+COALESCE((
+SELECT new_price
+FROM Products
+WHERE product_id = p.product_id AND change_date <= '2019-08-16'
+ORDER BY change_date DESC
+LIMIT 1
+), 10) AS price
+FROM Products p
+ORDER BY p.product_id;
