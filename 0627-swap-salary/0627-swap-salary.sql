9f90f10d456d2245b9075058296067666456e798
pavanthandrangi29 <114342432+pavanthandrangi29@users.noreply.github.com>
2025-09-01 23:06:17 +0530

    Time: 230 ms (83.09%), Space: 0B (100%) - LeetHub

---
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+UPDATE Salary
+SET sex = (CASE
+           WHEN sex = 'm'then'f'
+           else 'm'
+           END);
